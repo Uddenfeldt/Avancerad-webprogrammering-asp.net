6651730a39a9eb529328098e38743a0669e771f9
Marc Uddenfeldt <marcuddenfeldt@hotmail.com>
2017-11-27 13:53:45 +0100

    first commit

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input;
+            string sentenceTwo = ("");
+            bool useProgram = true;
+            
+            while (useProgram)
+            {
+                Console.Clear();
+                Console.WriteLine("Please enter a name separated by comma(e.g marc,johan,nicklas)");
+                input = Console.ReadLine();
+                sentenceTwo = input;
+                string[] split = sentenceTwo.Split(',');
+
+                foreach (string item in split)
+                {
+                    if (sentenceTwo.Length < 2 && sentenceTwo.Length > 9 == false)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("Please write between 2 and 9 letters");
+                    }
+                    else
+                    {
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.WriteLine("***SUPER-" + item.ToUpper().Trim() + "***");
+                    }
+                }
+                Console.ReadLine();
+                
+            }
+            
+            
+
+        }
+            
+    }
+}
