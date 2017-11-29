4974946c91f45fe86542d05c503efca0294b02ed
Marc Uddenfeldt <marcuddenfeldt@hotmail.com>
2017-11-29 11:33:28 +0100

    sad

---
+++ textfileproject/textfileproject/Controllers/PersonController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.IO;
+using textfileproject.Models;
+
+namespace textfileproject.Controllers
+{
+    [Route("Person")]
+    public class PersonController : Controller
+    {
+        [HttpGet,Route("FindCustomer")]
+        public IActionResult FindCustomer(int? id)
+        {
+            
+          
+            var customerList = new List<string>(System.IO.File.ReadAllLines(@"C:\Users\Marc\Desktop\Marc\VS grejjer\Avancerad webklientprogrammering asp.net\textfileproject\textfileproject\Data\TextFile.txt"));
+
+            if (id == null )
+            {
+                return BadRequest("Please type something between 1-7");
+            }
+
+            else if (id < 0)
+            {
+                return BadRequest("you cant put in minus");
+            }
+            foreach (var row in customerList)
+            {
+                return Ok(customerList);
+            }
+            
+            return Ok(customerList);
+        }
+    }
+}
--- textfileproject/textfileproject/wwwroot/js/JavaScript.js
+++ textfileproject/textfileproject/wwwroot/js/JavaScript.js
@@ -1,1 +1,26 @@
-﻿
+﻿function clear() {
+    $("#succesMessage").text("")
+    $("errorMessage").text("")
+}
+
+$("#knapp").click(function () {
+
+    let n = $("#enteredNumber").val();
+
+    $.ajax({
+        url: '/person/findcustomer',
+        method: 'GET',
+        data: { number: n }
+    })
+        .done(function (result) {
+            console.log("Succes", result)
+            clear()
+            $("#succesMessage").text(result)
+        })
+        .fail(function (xhr, status, error) {
+            console.log("Fail", xhr)
+            clear()
+            $("#errorMessage").text(xhr.responseText)
+        })
+
+});
